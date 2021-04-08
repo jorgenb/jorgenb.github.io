cbc4ab9e8123966ddad457c492a17bf022553d84
Jørgen Birkhaug <jorgen.birkhaug@uib.no>
2021-04-08 14:58:52 +0200

    SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Dykkeprat-arkivet' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
